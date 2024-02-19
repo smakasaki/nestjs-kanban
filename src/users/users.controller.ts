d98bd6224f8150824ad0ccb3a5e529981c32de12
Andrei Griu <salamandr258@gmail.com>
2024-02-20 00:13:42 +0200

    implement column's initialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,15 +15,26 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { ColumnsService } from 'src/columns/columns.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly columnsService: ColumnsService,
+  ) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() createUserDto: CreateUserDto) {
-    return await this.usersService.create(createUserDto);
+    const user = await this.usersService.create(createUserDto);
+    const columnsNames = ['backlog', 'todo', 'doing', 'done'];
+
+    columnsNames.forEach(async (name) => {
+      await this.columnsService.create({ name }, user._id);
+    });
+
+    return user;
   }
 
   @Get()
@@ -33,6 +44,7 @@ export class UsersController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   async findOne(@Param('id') id: string) {
     const user = await this.usersService.findOne(id);
     if (!user) {
@@ -42,11 +54,13 @@ export class UsersController {
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard)
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   async remove(@Param('id') id: string) {
     return await this.usersService.remove(id);
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from '../shared/schemas/user.schema';
+import { ColumnsModule } from 'src/columns/columns.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+    ColumnsModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
